86b4577986a0511da118a0fe2b36694c276c2ace
dhrions <aturing@laposte.net>
2024-01-14 11:22:11 +0100

    🎨 Create py & rename ipynb

---
+++ python/strings/format_string.py
@@ -0,0 +1,13 @@
+import re
+
+def kebab(s):
+    return '-'.join(
+        re.sub(r"(\s|_|-|/)+", " ",
+               re.sub(r"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+",
+                      lambda mo: ' ' + mo.group(0).lower(), s)).split()
+    ).replace("'", '').replace('.', '').replace('/', '').replace('’', '')
+
+def is_kebab_case(chaine):
+    # Vérifie si la chaîne est composée de lettres minuscules, chiffres et tirets uniquement,
+    # et si elle commence et se termine par une lettre minuscule.
+    return bool(re.match(r'^[a-z][a-z0-9-]*$', chaine))
