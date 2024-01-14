a494ab196a1097ecebe8d5c4bd5cae0417ca276c
dhrions <aturing@laposte.net>
2024-01-14 11:26:15 +0100

    🎨 Improve

--- python/strings/format_string.py
+++ python/strings/format_string.py
@@ -16,11 +16,18 @@ def convert_into_kebab_case(string):
     >>> convert_into_kebab_case('HelloWorld')
     'hello-world'    
     """
-    return '-'.join(
+
+    # Convert string to kebab case
+    result = '-'.join(
         re.sub(r"(\s|_|-|/)+", " ",
                re.sub(r"[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+",
                       lambda mo: ' ' + mo.group(0).lower(), string)).split()
-    ).replace("'", '').replace('.', '').replace('/', '').replace('’', '')
+    )
+
+    # Remove special characters
+    result = result.replace("'", '').replace('.', '').replace('/', '').replace('’', '')
+
+    return result
 
 def is_kebab_case(chaine):
     """
