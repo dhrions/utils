df19e0def2893955dafeed6bae1babe0b5999018
dhrions <aturing@laposte.net>
2025-03-07 20:46:58 +0100

    :sparkles: Update

--- python/click/main.py
+++ python/click/main.py
@@ -18,6 +18,10 @@ def dropdb():
 def whoami(count, name):
     for _ in range(count):
         click.echo('Hello %s!' % name)
+        
+@main.command()
+def command_in_four_parts():
+    click.echo('This is a command that spans four lines.')
 
 if __name__ == "__main__":
     main()
